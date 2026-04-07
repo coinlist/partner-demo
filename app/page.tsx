0a4cb70ad3ee6a85117ed3790e1a5d9612082b4d
Iliyan Germanov <iliyan@coinlist.co>
2026-04-07 14:48:41 +0300

    Fix white flashing

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { HomeContainer } from "@/features/home/HomeContainer";
 import { LoginContainer } from "@/features/login/LoginContainer";
 import { coinListServer } from "@/lib/coinlist-server";
 import { NO_OP_COOKIES_SINK } from "@/lib/session-store";
-import { Suspense } from "react";
 
 export default async function HomePage() {
   const coinlistServer = coinListServer(NO_OP_COOKIES_SINK);
@@ -10,10 +9,6 @@ export default async function HomePage() {
   if (loggedIn) {
     return <HomeContainer />;
   } else {
-    return (
-      <Suspense fallback={null}>
-        <LoginContainer />
-      </Suspense>
-    );
+    return <LoginContainer />;
   }
 }
