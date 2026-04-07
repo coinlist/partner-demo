2fa2447069b98c8de8ce1ff34cc118c68dc9235f
Iliyan Germanov <iliyan@coinlist.co>
2026-04-07 14:38:33 +0300

    Fix NextJS build error

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { HomeContainer } from "@/features/home/HomeContainer";
 import { LoginContainer } from "@/features/login/LoginContainer";
 import { coinListServer } from "@/lib/coinlist-server";
 import { NO_OP_COOKIES_SINK } from "@/lib/session-store";
+import { Suspense } from "react";
 
 export default async function HomePage() {
   const coinlistServer = coinListServer(NO_OP_COOKIES_SINK);
@@ -9,6 +10,10 @@ export default async function HomePage() {
   if (loggedIn) {
     return <HomeContainer />;
   } else {
-    return <LoginContainer />;
+    return (
+      <Suspense fallback={null}>
+        <LoginContainer />
+      </Suspense>
+    );
   }
 }
