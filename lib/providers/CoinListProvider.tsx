d8449965449b6f4cd5881d0e29be12343bf68259
Iliyan Germanov <iliyan@coinlist.co>
2026-04-24 14:47:41 +0300

    Remove the CORS workaround

--- lib/providers/CoinListProvider.tsx
+++ lib/providers/CoinListProvider.tsx
@@ -10,8 +10,6 @@ export function DemoCoinListProvider({
   children: React.ReactNode;
 }) {
   const config: ClientConfig = {
-    // TODO: Remove - temp workaround for a CORS issue in the Public API
-    baseUrl: "/api/coinlist/client",
     sandbox: true,
     clientId: ClientId(
       requiredEnv(
